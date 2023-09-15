801a93ee99f496aa714668052fffe3d79256032e
RabieImghi <rabieaitimghi7@gmail.com>
2023-09-15 09:49:47 +0200

    c : challenge 15

---
+++ ch15_c/challenge15.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+int main(){
+	char  carac;
+	printf("Donne un  caractere  : ");
+	carac = getchar();
+	if(carac<=90 && carac>=65){
+		printf(" est un alphabet majuscule ");
+	}else{
+		printf(" ne pas un alphabet majuscule ");
+	}
+	
+	
+
+	
+		
+}
