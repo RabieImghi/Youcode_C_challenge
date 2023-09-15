116b4074e1def0bf932d3583699b372d4692c59f
RabieImghi <rabieaitimghi7@gmail.com>
2023-09-15 17:06:30 +0200

    c : challenge 21

--- ch10_c/challenge1.cpp
+++ ch10_c/challenge1.cpp
@@ -4,13 +4,13 @@ int main(){
 	printf("donne un character : ");
 	ch = getchar();
 	switch(ch){
-		case 'a':printf("nombre est voyelle "); break;
-		case 'e':printf("nombre est voyelle "); break;
-		case 'i':printf("nombre est voyelle "); break;
-		case 'o':printf("nombre est voyelle "); break;
-		case 'y':printf("nombre est voyelle "); break;
-		case 'u':printf("nombre est voyelle "); break;
-		default  :printf("nombre non voyelle "); break;
+		case 'a':printf("%c est voyelle ",ch); break;
+		case 'e':printf("%c est voyelle ",ch); break;
+		case 'i':printf("%c est voyelle ",ch); break;
+		case 'o':printf("%c est voyelle ",ch); break;
+		case 'y':printf("%c est voyelle ",ch); break;
+		case 'u':printf("%c est voyelle ",ch); break;
+		default  :printf("%c non voyelle ",ch); break;
 	}
 	return 0;
 
---
+++ ch21_c/challenge21.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main(){
+	int n=8,i,j;
+	 for (i=1;i<=n;i++) {
+        for (j=1;j<=n-i;j++) {
+            printf(" ");
+        }
+        for (j= 1; j <= 2*i-1; j++) {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+	
+		
+}
