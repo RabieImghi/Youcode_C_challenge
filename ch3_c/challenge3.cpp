46695f28667dc0e665cf1a6228c0351450ddbf49
RabieImghi <rabieaitimghi7@gmail.com>
2023-09-15 09:10:22 +0200

    c : challenge 3

---
+++ ch3_c/challenge3.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a,b;
+	printf("donne a : ");
+	scanf("%d",&a);
+	printf("donne b : ");
+	scanf("%d",&b);
+	printf("\na + b = %d",a+b);
+	printf("\na - b = %d",a-b);
+	printf("\na / b = %d",a/b);
+	printf("\na * b = %d",a*b);
+	printf("\na mod 	b = %d",a%b);
+	return 0;
+}
