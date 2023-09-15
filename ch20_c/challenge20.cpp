262641656872a39669b002631b88be89842bd26d
RabieImghi <rabieaitimghi7@gmail.com>
2023-09-15 15:24:31 +0200

    c : challenge 20

---
+++ ch20_c/challenge20.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+int main(){
+	int number,i;
+	printf("donne un nombre : ");
+	scanf("%d",&number);
+	for(i=1;i<11;i++){
+		printf("%d * %d = %d\n",number,i,number*i);
+	}
+	
+	
+		
+}
