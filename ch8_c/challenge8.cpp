e10c8f41962b7ca3488248e3182fad9655feae1b
RabieImghi <rabieaitimghi7@gmail.com>
2023-09-15 09:34:19 +0200

    c : challenge 8

---
+++ ch8_c/challenge8.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+	int numberDes;
+	printf("donne un nombre dicemal : ");
+	scanf("%d",&numberDes);
+	printf("\nle nombre en octal est : %o",numberDes);
+	printf("\nle nombre hexaDesimal est : %x",numberDes);
+	return 0;
+}
