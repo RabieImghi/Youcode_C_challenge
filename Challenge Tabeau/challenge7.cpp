d4de1b98f2ef91809ee4d466d09258c42a19534d
RabieImghi <rabieaitimghi7@gmail.com>
2023-09-19 15:32:05 +0200

    challenge 7 les tableau

---
+++ Challenge Tabeau/challenge7.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int L,C,i;
+	do{
+		printf("Donne la dimension de table L (max 50): ");
+		scanf("%d",&L);
+		printf("Donne la dimension de table C (max 50): ");
+		scanf("%d",&C);	
+		if((C>50 || C<=0)) 	printf("C > 50 !! ");
+		if((L>50 || L<=0))	printf("L > 50 !! ");
+	}while((C>50 || C<=0) || (L>50 || L<=0));
+	char T[L][C];
+	for(i=0;i<sizeof(L);i++)
+		for(int j=0;j<sizeof(C);j++){
+			printf("la case T[%d][%d] = ",i+1,j+1);
+			scanf("%d",&T[i][j]);	
+		}
+	for(i=0;i<sizeof(L);i++){
+		for(int j=0;j<sizeof(C);j++){
+			printf("T[%d][%d] = %d\t",i+1,j+1,T[i][j]);
+		}
+		printf("\n");
+	}	
+}
